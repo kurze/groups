f3e6b9eddeca79e001e8e44fcf458d13e71ba9ac
Simon Kesteloot <skesteloot@wallix.com>
2025-10-01 22:31:24 +0200

    feat(models): export new security models
    Export new models in models/mod.rs:
    - AuthLog with event_types constants
    - PasswordResetToken
    - RateLimitRecord with action_types and thresholds
    
    Task: T009

--- src/db/models/mod.rs
+++ src/db/models/mod.rs
@@ -1,5 +1,11 @@
+mod auth_log;
 mod group;
+mod password_reset_token;
+mod rate_limit;
 mod user;
 
+pub use auth_log::{event_types, AuthLog};
 pub use group::Group;
+pub use password_reset_token::PasswordResetToken;
+pub use rate_limit::{action_types, thresholds, RateLimitRecord};
 pub use user::User;
